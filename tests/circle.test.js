8e7e3b13e076f80cfc70f951410f8a070b7a5bc8
Ajay S <ajaycshan@gmail.com>
2023-11-01 18:26:24 +0200

    Amend tests to ensure they've passed

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,1 +1,12 @@
 const Circle = require('../lib/circle.js');
+
+describe('classCircle', () => {
+    it('should produce square logo with user desired text, text color and shape color', () => {
+        const circle = new Circle('GHI', 'gray','red');
+        expect(circle.render()).toEqual(`
+        <svg width="400" height="200">
+            <circle cx="200" cy="100" r="100" fill="red"/>
+            <text x="200" y="125" font-size="80" text-anchor="middle" fill="gray">GHI</text>
+        </svg>`);
+    });
+});
