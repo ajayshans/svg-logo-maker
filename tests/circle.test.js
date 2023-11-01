584df85cbdff560f41b619572750cefb95fbe6d6
Ajay S <ajaycshan@gmail.com>
2023-11-01 18:52:42 +0200

    Amend pixels for circle

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -4,8 +4,8 @@ describe('classCircle', () => {
     it('should produce square logo with user desired text, text color and shape color', () => {
         const circle = new Circle('GHI', 'gray','red');
         expect(circle.render()).toEqual(`
-        <svg width="400" height="200">
-            <circle cx="200" cy="100" r="100" fill="red"/>
+        <svg width="300" height="200">
+            <circle cx="150" cy="100" r="100" fill="red"/>
             <text x="200" y="125" font-size="80" text-anchor="middle" fill="gray">GHI</text>
         </svg>`);
     });
