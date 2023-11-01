e3e20ae06b12f16614f5cfede38178099e6c74bc
Ajay S <ajaycshan@gmail.com>
2023-11-01 18:55:12 +0200

    Amend pixels for square

--- tests/square.test.js
+++ tests/square.test.js
@@ -4,9 +4,9 @@ describe('classSquare', () => {
     it('should produce square logo with user desired text, text color and shape color', () => {
         const square = new Square('DEF', 'black','green');
         expect(square.render()).toEqual(`
-        <svg width="400" height="200">
-            <rect x="100" y="10" width="200" height="200" fill="green"/>
-            <text x="200" y="125" font-size="80" text-anchor="middle" fill="black">DEF</text>
+        <svg width="300" height="200">
+            <rect x="50" y="10" width="200" height="200" fill="green"/>
+            <text x="150" y="125" font-size="80" text-anchor="middle" fill="black">DEF</text>
         </svg>`);
     });
 });
